f1556427f83f2bb6d9bd88768580e28d5a8e74be
Fozkun <s3924683@rmit.edu.vn>
2023-05-07 15:05:05 +0700

    Updated folder structures and files

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const path = require('path')
-const express = require('express')
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
 
-const app = express()
+require("./server/database/db");
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use((req,res)=>{
-    res.status(404)
-    res.send(`<h1>Error 404: Resource not found!</h1>`)
-})
-app.listen(3000,()=>{
-    console.log('App listening to port 3000')
-})
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res) => {
+  res.status(404);
+  res.send(`<h1>Error 404: Resource not found!</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log("App listening to port 3000");
+});
