ab2f966518803aaca3404f8be6516b9877430b57
Fozkun <s3924683@rmit.edu.vn>
2023-05-07 14:03:26 +0700

    updated

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use((req,res)=>{
+    res.status(404)
+    res.send(`<h1>Error 404: Resource not found!</h1>`)
+})
+app.listen(3000,()=>{
+    console.log('App listening to port 3000')
+})
