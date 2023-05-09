d9c2257328e670c8c8b9b6f47967cf3ff6450391
Fozkun <s3924683@rmit.edu.vn>
2023-05-09 07:55:26 +0700

    updated

--- server/database/mongoose.js
+++ server/database/mongoose.js
@@ -1,2 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_URI).then(() => console.log("DB Connected!"));
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => console.error(err));
+
+const User = require("../user.js");
+
+const user = new User({ name: "s3924683", age: 20 });
+user.save().then(console.log("Done!"));
