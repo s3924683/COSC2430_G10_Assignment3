3a20c90953985b0473e9398421f69809bc887234
Fozkun <s3924683@rmit.edu.vn>
2023-05-15 22:56:44 +0700

    updated

--- server/users.js
+++ server/users.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
-const validator = require('validator').default;
+const validator = require("validator").default;
 
 const usernameRules = {
   type: String,
   unique: true,
   required: true,
   trim: true,
+  maxLength: 15,
+  minLength: 8,
+  match: /^[a-zA-Z0-9]+$/,
   validate(value) {
-    if (!validator.isAlphanumeric(value, 'pl-PL')) {
-      throw new Error('Name cannot contain special characters.')
+    if (!validator.isAlphanumeric(value, "pl-PL")) {
+      throw new Error("Name cannot contain special characters.");
     }
-  }
-}
+  },
+};
 const passwordRules = {
   type: String,
   required: true,
   validate(value) {
-    console.log(value)
+    console.log(value);
     if (value !== this.password2) {
-      throw new Error("Passwords don't match. Try again.")
+      throw new Error("Passwords don't match. Try again.");
     }
 
     if (value.length < 8) {
-      throw new Error("Passwords is too short. At least 8 characters.")
+      throw new Error("Passwords is too short. At least 8 characters.");
     }
-  }
-}
+  },
+};
 const pfpRules = {
   data: Buffer,
-  contentType: String
-}
+  contentType: String,
+};
 
 const userSchema = new mongoose.Schema({
   username: usernameRules,
@@ -55,5 +58,5 @@ const vendorSchema = new mongoose.Schema({
 module.exports = {
   User: mongoose.model("User", userSchema),
   Shipper: mongoose.model("Shipper", shipperSchema),
-  Vendor: mongoose.model("Vendor", vendorSchema)
-}
+  Vendor: mongoose.model("Vendor", vendorSchema),
+};
