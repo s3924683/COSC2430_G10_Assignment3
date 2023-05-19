01f8f0cdd678aa8f54e8c30b083c5dd3ae11fdac
Fozkun <fozkun230403@gmail.com>
2023-05-19 18:41:55 +0700

    added more stuff

--- public/js/userSignupValidator.js
+++ public/js/userSignupValidator.js
@@ -1,89 +1,90 @@
-const signupForm = document.getElementById("signup-form")
-const usernameInput = document.getElementById("username")
-const passwordInput = document.getElementById("password")
-const nameInput = document.getElementById("name")
-const addressInput = document.getElementById("address")
+const signupForm = document.getElementById("signup-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const nameInput = document.getElementById("name");
+const addressInput = document.getElementById("address");
 
-signupForm.addEventListener('submit', (ev) => {
-    ev.preventDefault()
-    validateInputs();
-})
+signupForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+
+  if (validateInputs()) {
+    HTMLFormElement.prototype.submit.call(signupForm);
+  }
+});
 
 const setError = (element, message) => {
-    const formGroup = element.parentElement;
-    console.log(formGroup)
-    const errorDisplay = formGroup.querySelector(".error-message");
+  const formGroup = element.parentElement;
+  const errorDisplay = formGroup.querySelector(".error-message");
 
-    console.log(errorDisplay)
-    errorDisplay.innerText = message;
+  errorDisplay.innerText = message;
 
-    formGroup.classList.add("error");
-    formGroup.classList.remove("success");
+  formGroup.classList.add("error");
+  formGroup.classList.remove("success");
 };
 const setSuccess = (element) => {
-    const formGroup = element.parentElement;
-    const errorDisplay = formGroup.querySelector(".error-message");
+  const formGroup = element.parentElement;
+  const errorDisplay = formGroup.querySelector(".error-message");
 
-    errorDisplay.innerText = "";
+  errorDisplay.innerText = "";
 
-    formGroup.classList.remove("error");
-    formGroup.classList.add("success");
+  formGroup.classList.remove("error");
+  formGroup.classList.add("success");
 };
 
 const validateInputs = () => {
-    let errors = 0;
-    const username = usernameInput.value.trim()
-    const password = passwordInput.value.trim()
-    const name = nameInput.value.trim()
-    const address = addressInput.value.trim()
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+  const name = nameInput.value.trim();
+  const address = addressInput.value.trim();
 
-    console.log(signupForm)
-    
-    if (username === "") {
-        setError(usernameInput, "Username name is required!");
-        errors += 1
-    } else if (username.length < 8 || username.length > 15) {
-        setError(usernameInput, "Username must be a length from 8 to 15!");
-        errors += 1
-    }
-    else if (!username.match(/^[a-zA-Z0-9]+$/)) {
-        setError(usernameInput, "Username cannot contain special characters.");
-        errors += 1
-    }
-    else {
-        setSuccess(usernameInput);
-    }
+  let isValid = true;
 
-    if (password == "") {
-        setError(passwordInput, "Password is required!");
-        errors += 1
-    } else if (password.length < 8 || password.length > 20) {
-        setError(passwordInput, "Password length must be 8 to 20 characters!");
-        errors += 1
-    }
-    else if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/)) {
-        setError(passwordInput, "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character!");
-        errors += 1
-    }
-    else {
-        setSuccess(passwordInput);
-    }
+  if (username === "") {
+    setError(usernameInput, "Username is required!");
+    isValid = false;
+  } else if (username.length < 8 || username.length > 15) {
+    setError(usernameInput, "Username must be a length from 8 to 15!");
+    isValid = false;
+  } else if (!username.match(/^[a-zA-Z0-9]+$/)) {
+    setError(usernameInput, "Username cannot contain special characters.");
+    isValid = false;
+  } else {
+    setSuccess(usernameInput);
+  }
 
-    if (name === "") {
-        setError(nameInput, "Name is required!");
-        errors += 1
-    } else {
-        setSuccess(nameInput);
-    }
+  if (password == "") {
+    setError(passwordInput, "Password is required!");
+    errors += 1;
+  } else if (password.length < 8 || password.length > 20) {
+    setError(passwordInput, "Password length must be 8 to 20 characters!");
+    errors += 1;
+  } else if (
+    !password.match(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/
+    )
+  ) {
+    setError(
+      passwordInput,
+      "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character!"
+    );
+    errors += 1;
+  } else {
+    setSuccess(passwordInput);
+  }
 
-    if (address === "") {
-        setError(addressInput, "Address is required!");
-        errors += 1
-    } else {
-        setSuccess(nameInput);
-    }
+  if (name === "") {
+    setError(nameInput, "Name is required!");
+    errors += 1;
+  } else {
+    setSuccess(nameInput);
+  }
 
-    if (errors == 0) {
-        signupForm.submit()
-    }
+  if (address === "") {
+    setError(addressInput, "Address is required!");
+    errors += 1;
+  } else {
+    setSuccess(nameInput);
+  }
+
+  return isValid;
 };
--- public/js/vendorSignupValidator.js
+++ public/js/vendorSignupValidator.js
@@ -1,88 +1,101 @@
-const signupForm = document.getElementById("signup-form")
-const usernameInput = document.getElementById("username")
-const passwordInput = document.getElementById("password")
-const businessNameInput = document.getElementById("businessName")
-const businessAddressInput = document.getElementById("businessAddress")
+const signupForm = document.getElementById("signup-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const businessNameInput = document.getElementById("businessName");
+const businessAddressInput = document.getElementById("businessAddress");
 
-signupForm.addEventListener('submit', (ev) => {
-    ev.preventDefault()
-    validateInputs();
-})
+signupForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  if (validateInputs()) {
+    HTMLFormElement.prototype.submit.call(signupForm);
+  }
+});
 
 const setError = (element, message) => {
-    const formGroup = element.parentElement;
-    console.log(formGroup)
-    const errorDisplay = formGroup.querySelector(".error-message");
+  const formGroup = element.parentElement;
+  const errorDisplay = formGroup.querySelector(".error-message");
 
-    console.log(errorDisplay)
-    errorDisplay.innerText = message;
+  errorDisplay.innerText = message;
 
-    formGroup.classList.add("error");
-    formGroup.classList.remove("success");
+  formGroup.classList.add("error");
+  formGroup.classList.remove("success");
 };
 const setSuccess = (element) => {
-    const formGroup = element.parentElement;
-    const errorDisplay = formGroup.querySelector(".error-message");
+  const formGroup = element.parentElement;
+  const errorDisplay = formGroup.querySelector(".error-message");
 
-    errorDisplay.innerText = "";
+  errorDisplay.innerText = "";
 
-    formGroup.classList.remove("error");
-    formGroup.classList.add("success");
+  formGroup.classList.remove("error");
+  formGroup.classList.add("success");
 };
 
 const validateInputs = () => {
-    let errors = 0;
-    const username = usernameInput.value.trim()
-    const password = passwordInput.value.trim()
-    const businessName = businessNameInput.value.trim()
-    const businessAddress = businessAddressInput.value.trim()
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+  const businessName = businessNameInput.value.trim();
+  const businessAddress = businessAddressInput.value.trim();
 
-    if (username === "") {
-        setError(usernameInput, "Username name is required!");
-        errors += 1
-    } else if (username.length < 8 || username.length > 15) {
-        setError(usernameInput, "Username must be a length from 8 to 15!");
-        errors += 1
-    }
-    else if (!username.match(/^[a-zA-Z0-9]+$/)) {
-        setError(usernameInput, "Username cannot contain special characters.");
-        errors += 1
-    }
-    else {
-        setSuccess(usernameInput);
-    }
+  let isValid = true;
+  if (username === "") {
+    setError(usernameInput, "Username name is required!");
+    isValid = false;
+  } else if (username.length < 8 || username.length > 15) {
+    setError(usernameInput, "Username must be a length from 8 to 15!");
+    isValid = false;
+  } else if (!username.match(/^[a-zA-Z0-9]+$/)) {
+    setError(usernameInput, "Username cannot contain special characters.");
+    isValid = false;
+  } else {
+    setSuccess(usernameInput);
+  }
 
-    if (password == "") {
-        setError(passwordInput, "Password is required!");
-        errors += 1
-    } else if (password.length < 8 || password.length > 20) {
-        setError(passwordInput, "Password length must be 8 to 20 characters!");
-        errors += 1
-    }
-    else if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/)) {
-        setError(passwordInput, "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character!");
-        errors += 1
-    }
-    else {
-        setSuccess(passwordInput);
-    }
+  if (password == "") {
+    setError(passwordInput, "Password is required!");
+    isValid = false;
+  } else if (password.length < 8 || password.length > 20) {
+    setError(passwordInput, "Password length must be 8 to 20 characters!");
+    isValid = false;
+  } else if (
+    !password.match(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/
+    )
+  ) {
+    setError(
+      passwordInput,
+      "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character!"
+    );
+    isValid = false;
+  } else {
+    setSuccess(passwordInput);
+  }
 
-    if (businessName === "") {
-        setError(businessNameInput, "Business name is required!");
-        errors += 1
-    } else {
-        setSuccess(businessNameInput);
-    }
+  if (businessName == "") {
+    setError(businessNameInput, "Business name is required!");
+    isValid = false;
+  }
+  else if (businessName.length < 5) {
+    setError(
+      businessNameInput,
+      "Business name must be more than 5 characters!"
+    );
+    isValid = false;
+  } else {
+    setSuccess(businessNameInput);
+  }
 
-    if (businessAddress === "") {
-        setError(businessAddressInput, "Business address is required!");
-        errors += 1
-    } else {
-        setSuccess(businessAddressInput);
-    }
+  if (businessAddress === "") {
+    setError(businessAddressInput, "Business address is required!");
+    isValid = false;
+  } else if (businessAddress.length < 5) {
+    setError(
+      businessAddressInput,
+      "Business address must be more than 5 characters!"
+    );
+    isValid = false;
+  } else {
+    setSuccess(businessAddressInput);
+  }
 
-    if (errors == 0) {
-        console.log('hi')
-        signupForm.submit()
-    }
+  return isValid;
 };
